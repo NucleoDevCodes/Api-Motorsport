916d127dd522e9bc61a3cfe8e7fc017e6b9611f7
Otavio <estudosprogotavio@gmail.com>
2025-08-14 09:08:17 -0300

    TASK01- feat: Adicionando ApiMotorsportApplication

---
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/ApiMotorsportApplication.java
@@ -0,0 +1,13 @@
+package devCodes.Zerphyis.ApiMotorsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMotorsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMotorsportApplication.class, args);
+	}
+
+}
