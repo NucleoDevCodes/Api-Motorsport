9f7cd2540beafef402be40b3031d966a6c441d44
Otavio <estudosprogotavio@gmail.com>
2025-08-19 07:17:32 -0300

    TASK09 - feat:adicionando classe NotFoundException

---
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/Infra/Exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package devCodes.Zerphyis.ApiMotorsport.Infra.Exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
