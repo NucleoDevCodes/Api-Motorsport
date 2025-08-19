c351359f721862b056925e45d4ec9459adcf7ad4
Otavio <estudosprogotavio@gmail.com>
2025-08-19 07:19:09 -0300

    TASK09 - feat:adicionando ControllerEspecificacaoController

---
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/Infra/Controllers/ControllerEspecificacaoController.java
@@ -0,0 +1,42 @@
+package devCodes.Zerphyis.ApiMotorsport.Infra.Controllers;
+
+import devCodes.Zerphyis.ApiMotorsport.Application.Records.DataEspecificacaoTecnicaRequest;
+import devCodes.Zerphyis.ApiMotorsport.Application.Records.DataEspecificacaoTecnicaResponse;
+import devCodes.Zerphyis.ApiMotorsport.Application.Services.ServiceEspecificacaoTecnica;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/especificacao")
+@RequiredArgsConstructor
+public class ControllerEspecificacaoController {
+
+    private final ServiceEspecificacaoTecnica service;
+
+    @PostMapping
+    public ResponseEntity<DataEspecificacaoTecnicaResponse> criar(@Valid @RequestBody DataEspecificacaoTecnicaRequest dto) {
+        return ResponseEntity.ok(service.criar(dto));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DataEspecificacaoTecnicaResponse> atualizar(
+            @PathVariable Long id,
+            @Valid @RequestBody DataEspecificacaoTecnicaRequest dto) {
+        return ResponseEntity.ok(service.atualizar(id, dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataEspecificacaoTecnicaResponse> buscarId(@PathVariable Long id){
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
