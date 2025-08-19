fdf1d16867031c0e647de5e2ceaf0d3c62f2cdfd
Otavio <estudosprogotavio@gmail.com>
2025-08-19 07:18:51 -0300

    TASK09 - feat:adicionando ServiceEspecificacaoTecnica

---
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/Application/Services/ServiceEspecificacaoTecnica.java
@@ -0,0 +1,81 @@
+package devCodes.Zerphyis.ApiMotorsport.Application.Services;
+
+import devCodes.Zerphyis.ApiMotorsport.Application.Records.DataEspecificacaoTecnicaRequest;
+import devCodes.Zerphyis.ApiMotorsport.Application.Records.DataEspecificacaoTecnicaResponse;
+import devCodes.Zerphyis.ApiMotorsport.Infra.Exceptions.EntityNotFoundException;
+import devCodes.Zerphyis.ApiMotorsport.Infra.Exceptions.NotFoundException;
+import devCodes.Zerphyis.ApiMotorsport.Model.Entity.Carro.Carro;
+import devCodes.Zerphyis.ApiMotorsport.Model.Entity.Especificao.EspecificacaoTecnica;
+import devCodes.Zerphyis.ApiMotorsport.Model.Repositorys.CarroRepository;
+import devCodes.Zerphyis.ApiMotorsport.Model.Repositorys.EspecificacaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class ServiceEspecificacaoTecnica {
+
+    private final EspecificacaoRepository especificacaoTecnicaRepository;
+    private final CarroRepository carroRepository;
+
+    public DataEspecificacaoTecnicaResponse criar(DataEspecificacaoTecnicaRequest dto) {
+        Carro carro = carroRepository.findById(dto.carroId()).orElseThrow(() -> new NotFoundException("Carro não encontrado com id: " + dto.carroId()));
+
+        EspecificacaoTecnica especificacao = new EspecificacaoTecnica();
+
+        especificacao.setCarro(carro);
+        especificacao.setTitulo(dto.titulo());
+        especificacao.setValor(dto.valor());
+
+        especificacao = especificacaoTecnicaRepository.save(especificacao);
+
+        return toResponseDTO(especificacao);
+    }
+
+    public DataEspecificacaoTecnicaResponse atualizar(Long id,DataEspecificacaoTecnicaRequest dto){
+        EspecificacaoTecnica especificacao = especificacaoTecnicaRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Especificação não encontrada com id: " + id));
+
+        Carro carro = carroRepository.findById(dto.carroId())
+                .orElseThrow(() -> new NotFoundException("Carro não encontrado com id: " + dto.carroId()));
+
+        especificacao.setCarro(carro);
+        especificacao.setTitulo(dto.titulo());
+        especificacao.setValor(dto.valor());
+
+        especificacao = especificacaoTecnicaRepository.save(especificacao);
+
+        return toResponseDTO(especificacao);
+    }
+
+
+    public DataEspecificacaoTecnicaResponse buscarPorId(Long id) {
+        EspecificacaoTecnica especificacao = especificacaoTecnicaRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Especificação não encontrada com id: " + id));
+
+        return toResponseDTO(especificacao);
+    }
+
+    public void deletar(Long id){
+        EspecificacaoTecnica especificacao=especificacaoTecnicaRepository.findById(id).orElseThrow(() -> new NotFoundException("Especificação não encontrada com id: " + id));
+
+    especificacaoTecnicaRepository.delete(especificacao);
+    }
+
+
+
+    private DataEspecificacaoTecnicaResponse toResponseDTO(EspecificacaoTecnica especificacao) {
+        Carro carro = especificacao.getCarro();
+        return new DataEspecificacaoTecnicaResponse(
+                especificacao.getTitulo(),
+                especificacao.getValor(),
+                carro.getModelo(),
+                carro.getNome(),
+                carro.getDescricao(),
+                carro.getImagemUrl()
+        );
+    }
+}
