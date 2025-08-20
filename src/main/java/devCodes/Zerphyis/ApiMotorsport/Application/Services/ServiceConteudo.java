8db2b49885925846c34d2eaa1dde6924d1722e6e
Otavio <estudosprogotavio@gmail.com>
2025-08-20 10:27:41 -0300

    TASK10 - refactor:refatorando para boas praticas ServiceConteudo

--- src/main/java/devCodes/Zerphyis/ApiMotorsport/Application/Services/ServiceConteudo.java
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/Application/Services/ServiceConteudo.java
@@ -7,36 +7,46 @@ import devCodes.Zerphyis.ApiMotorsport.Model.Entity.Conteudo.Conteudo;
 import devCodes.Zerphyis.ApiMotorsport.Model.Repositorys.ConteudoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ServiceConteudo {
 
+
     private final ConteudoRepository repository;
 
-    public DataConteudoResponse getConteudo(Long id) {
-        Conteudo conteudo = repository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Conteúdo com ID " + id + " não encontrado"));
-        return toResponse(conteudo);
-    }
-
-    public DataConteudoResponse atualizarConteudo(Long id, DataConteudoRequest dto) {
-        Conteudo conteudo = repository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Conteúdo com ID " + id + " não encontrado"));
-
-        conteudo.setTitulo(dto.titulo());
-        conteudo.setConteudo(dto.conteudo());
-        conteudo.setImagemUrl(dto.imagemUrl());
-
-        return toResponse(repository.save(conteudo));
-    }
-
-    public DataConteudoResponse adicionarConteudo(DataConteudoRequest dto) {
+    @Transactional
+    public DataConteudoResponse create(DataConteudoRequest dto) {
         Conteudo conteudo = new Conteudo();
         conteudo.setTitulo(dto.titulo());
         conteudo.setConteudo(dto.conteudo());
         conteudo.setImagemUrl(dto.imagemUrl());
         return toResponse(repository.save(conteudo));
+    }
+
+    @Transactional
+    public DataConteudoResponse update(Long id, DataConteudoRequest dto) {
+        Conteudo conteudo = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Conteúdo não encontrado com ID " + id));
+        conteudo.setTitulo(dto.titulo());
+        conteudo.setConteudo(dto.conteudo());
+        conteudo.setImagemUrl(dto.imagemUrl());
+        return toResponse(repository.save(conteudo));
+    }
+
+    public DataConteudoResponse findById(Long id) {
+        Conteudo conteudo = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Conteúdo não encontrado com ID " + id));
+        return toResponse(conteudo);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if(!repository.existsById(id)) {
+            throw new NotFoundException("Conteúdo não encontrado com ID " + id);
+        }
+        repository.deleteById(id);
     }
 
     private DataConteudoResponse toResponse(Conteudo conteudo) {
