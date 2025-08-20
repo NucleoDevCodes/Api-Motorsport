3ca6986fb7fb5569637cdc9d7443ba3431b6f53d
Otavio <estudosprogotavio@gmail.com>
2025-08-20 10:14:34 -0300

    TASK10 - refactor:movendo DataCarroRequest para pacote Records

--- src/main/java/devCodes/Zerphyis/ApiMotorsport/Application/Records/Carro/DataCarroRequest.java
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/Application/Records/Carro/DataCarroRequest.java
@@ -1,4 +1,4 @@
-package devCodes.Zerphyis.ApiMotorsport.Application.Records;
+package devCodes.Zerphyis.ApiMotorsport.Application.Records.Carro;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
