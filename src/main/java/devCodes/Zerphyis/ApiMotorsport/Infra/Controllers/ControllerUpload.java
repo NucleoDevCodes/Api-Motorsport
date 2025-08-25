318abd9c40fd4fcbf672ea86fe728a0f7ea6056d
Otavio <estudosprogotavio@gmail.com>
2025-08-25 08:35:22 -0300

    TASK11 - feat:adicionando metodo getUpload,getAllUploads em ControllerUpload

--- src/main/java/devCodes/Zerphyis/ApiMotorsport/Infra/Controllers/ControllerUpload.java
+++ src/main/java/devCodes/Zerphyis/ApiMotorsport/Infra/Controllers/ControllerUpload.java
@@ -1,17 +1,16 @@
 package devCodes.Zerphyis.ApiMotorsport.Infra.Controllers;
 
+import devCodes.Zerphyis.ApiMotorsport.Application.Records.Upload.ResponseUpload;
 import devCodes.Zerphyis.ApiMotorsport.Application.Services.ServiceUpload;
 import devCodes.Zerphyis.ApiMotorsport.Model.Entity.Upload.Upload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/upload")
@@ -21,8 +20,21 @@ public class ControllerUpload {
     private final ServiceUpload service;
 
     @PostMapping
-    public ResponseEntity<Upload> uploadFile(@RequestParam("file") MultipartFile file) {
-        Upload upload = service.saveFile(file);
+    public ResponseEntity<ResponseUpload> uploadFile(@RequestParam("file") MultipartFile file) {
+        ResponseUpload upload = service.saveFile(file);
         return ResponseEntity.status(HttpStatus.CREATED).body(upload);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseUpload> getUpload(@PathVariable Long id) {
+        ResponseUpload upload = service.findById(id);
+        return ResponseEntity.ok(upload);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseUpload>> getAllUploads() {
+        List<ResponseUpload> uploads = service.findAll();
+        return ResponseEntity.ok(uploads);
+    }
+
 }
